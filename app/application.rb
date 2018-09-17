d893ea8454222bdc0588b1ffbf12d7207a57c86e
Wendy Calderon <wendycalderon@gmail.com>
2018-09-17 20:19:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,8 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    time = Time.now.strftime("%I:%M:%S %Z %z") 
-    new_time = time.split(":")
-    
-    if new_time[0].to_i < 12
+    time = Time.now.hour
+    if time < 12
       resp.write "Good Morning!"
       
     else
