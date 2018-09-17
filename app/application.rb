2ee1a537d1d6332e1612641eb50e0ca58c42525a
Wendy Calderon <wendycalderon@gmail.com>
2018-09-17 19:39:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Application
  
   def call(env)
     resp = Rack::Response.new
     time = Time.now.strftime("%I:%M:%S %Z %z") 
     time.split(":")
+    binding.pry
     if time[0].to_i < 12
       resp.write "Good Morning!"
     else
